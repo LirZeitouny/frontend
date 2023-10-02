d5c960a6add5db17ae8c5e89e6a5dd9ad34d024b
Lir Zeitouny <lirzetouny@gmail.com>
2023-10-02 18:55:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const token = uuidv4();
 
 function App() {
+  const [input, setInput] = useState('');
+  const [message, setMessage] = useState('');
+
+  function handleInputChanged(event) {
+    const value = event.target.value;
+    if (value.length < 10) setInput(value);
+    else setMessage('TOO LONG NUMBER INPUT');
+  }
+
+  async function getIsPrimeFromAPI() {
+    const requestBody = JSON.stringify({ input, token });
+
+    try {
+      const response = await fetch('http://localhost:8080/api/isPrime', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: requestBody,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setMessage(data);
+      } else {
+        setMessage('ERROR MAKING REQUEST');
+        console.error('Error making POST request:', response.statusText);
+      }
+    } catch (error) {
+      setMessage('ERROR MAKING REQUEST');
+      console.error('Error making POST request:', error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <TextField
+          label='Enter a number'
+          variant='outlined'
+          style={{ marginBottom: '20px' }}
+          value={input}
+          onChange={handleInputChanged}
+        />
+        <Button variant='contained' onClick={getIsPrimeFromAPI}>
+          TEST
+        </Button>
+        <Typography variant='h6' style={{ marginTop: '20px' }}>
+          {message}
+        </Typography>
       </header>
     </div>
   );
